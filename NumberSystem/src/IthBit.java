cbc7926c5474ee48422e81a07cd3f32c618f81ff
Arpit261j <Arpitgupta261j@gmail.com>
2023-11-29 00:48:52 +0530

    number system continued

---
+++ NumberSystem/src/IthBit.java
@@ -0,0 +1,10 @@
+public class IthBit {
+    public static void main(String[] args) {
+        if ((182&(1<<4))>0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+    }
+}
