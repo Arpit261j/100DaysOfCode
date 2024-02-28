451637e24c9d74e7a0e1cadf0743970060f58b10
Arpit261j <Arpitgupta261j@gmail.com>
2024-02-28 22:58:23 +0530

    ApreceedsB

---
+++ Leetcode/src/ApreceedsB.java
@@ -0,0 +1,26 @@
+public class ApreceedsB {
+    public static void main(String[] args) {
+        String s="aaab";
+        System.out.println(solution(s));
+    }
+    public static boolean solution(String s) {
+        int num=0;
+        int count=0;
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i)=='b') {
+                num=i;
+                count=1;
+                break;
+            }
+        }
+        if(count==0) {
+            return true;
+        }
+        for(int i=s.length()-1;i>num;i--) {
+            if(s.charAt(i)=='a') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
