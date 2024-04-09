9ef4c09ffd4d91cc04e2d1512b7dfd677f8b19bf
Arpit261j <Arpitgupta261j@gmail.com>
2024-04-09 11:53:15 +0530

    Multithreading Runnable

--- Multithreading/src/com/jobready/threading/Application.java
+++ Multithreading/src/com/jobready/threading/Application.java
@@ -1,4 +1,33 @@
 package com.jobready.threading;
 
 public class Application {
+    public static void main(String[] args) {
+
+        System.out.println("Started Thread A");
+        Task taskRunner = new Task("Thread-A");
+        taskRunner.start();
+
+        System.out.println("Started Thread B");
+        Task taskRunner2 = new Task("Thread-B");
+        taskRunner2.start();
+    }
+}
+class Task extends Thread {
+    String name;
+
+    Task(String name){
+        this.name=name;
+    }
+
+    public void run() {
+        Thread.currentThread().setName(name);
+        for(int i=0;i<1000;i++) {
+            System.out.println("Nums :" + i + " - "+ Thread.currentThread().getName());
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
