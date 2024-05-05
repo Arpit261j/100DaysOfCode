c56cc78f9f0ba08d58b5cdd7226856e795cc8cbe
Arpit261j <Arpitgupta261j@gmail.com>
2024-05-05 22:10:14 +0530

    SpecialArrayWithXElements

--- Leetcode/src/SpecialArrayWithXElements.java
+++ Leetcode/src/SpecialArrayWithXElements.java
@@ -1,6 +1,6 @@
-public class test {
+public class SpecialArrayWithXElements {
     public static void main(String[] args) {
-        int[] arr={0,0};
+        int[] arr={0,4,3,0,4};
         System.out.println(specialArray(arr));
     }
     public static int specialArray(int[] arr) {
@@ -8,7 +8,7 @@ public class test {
             return 0;
         }
         int start=0,end=arr.length;
-        while(start<end) {
+        while(start<=end) {
             int mid=start+(end-start)/2;
             int count=0;
             for(int num:arr) {
@@ -16,13 +16,14 @@ public class test {
                     count++;
                 }
             }
-            if(mid<=count) {
+            if(mid<count) {
                 start=mid+1;
+            } else if (mid==count) {
+                return mid;
             } else {
                 end=mid-1;
             }
         }
-
-        return end;
+        return -1;
     }
 }
