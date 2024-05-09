3ec307c0efc572c43c5e3a7ba52578d1be6a7a2b
Arpit261j <Arpitgupta261j@gmail.com>
2024-05-09 22:13:02 +0530

    CountingWordsWithPrefix

--- Leetcode/src/CountingWordsWithprefix.java
+++ Leetcode/src/CountingWordsWithprefix.java
@@ -1,14 +1,14 @@
-public class test {
-    public static void main(String[] args) {
-        String[] s={"i","p","pet","fxai","ortqpwkukg","rxgh","ylfh"};
-        System.out.println(prefixCount(s,"ikwjoty"));
+public class CountingWordsWithprefix {
+    public static void main(String[] args) {git status
+        String[] s= {"pay","attention","practice","attend"};
+        System.out.println(prefixCount(s,"at"));
     }
     public static int prefixCount(String[] words, String pref) {
         int count=0,count1=0;
         for(String word : words) {
             count1=0;
             for(int i=0;i<pref.length();i++) {
-                if(pref.charAt(i)==word.charAt(i)) {
+                if(word.length()>=pref.length() && pref.charAt(i)==word.charAt(i)) {
                     count1++;
                 } else {
                     break;
