e32ece9c9aea3fb8cf62554256f43818cadcee02
Arpit261j <Arpitgupta261j@gmail.com>
2024-05-10 23:59:26 +0530

    THreeSum

--- Leetcode/src/test.java
+++ Leetcode/src/test.java
@@ -1,23 +1,38 @@
+import java.util.ArrayList;
+import java.util.*;
+
 public class test {
     public static void main(String[] args) {
-        String[] s={"i","p","pet","fxai","ortqpwkukg","rxgh","ylfh"};
-        System.out.println(prefixCount(s,"ikwjoty"));
+        int[] s={-1,0,1,2,-1,-4};
+        System.out.println(threeSum(s));
     }
-    public static int prefixCount(String[] words, String pref) {
-        int count=0,count1=0;
-        for(String word : words) {
-            count1=0;
-            for(int i=0;i<pref.length();i++) {
-                if(pref.charAt(i)==word.charAt(i)) {
-                    count1++;
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans= new ArrayList<>();
+        for(int k=0;k<nums.length-2;k++) {
+            if(k>0 && nums[k]==nums[k-1]) {
+                continue;
+            }
+            int i=k+1,j=nums.length-1;
+            while(i<j){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0) {
+                    ans.add(Arrays.asList(nums[k],nums[i],nums[j]));
+                    while(i<j && nums[i]==nums[i+1]) {
+                        i++;
+                    }
+                    while(i<j && nums[j]==nums[j-1]) {
+                        j--;
+                    }
+                    i++;
+                    j--;
+                } else if (sum>0) {
+                    j--;
                 } else {
-                    break;
+                    i++;
                 }
             }
-            if(count1==pref.length()) {
-                count++;
-            }
         }
-        return count;
+        return ans;
     }
 }
